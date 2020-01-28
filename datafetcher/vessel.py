1289a459f7729ab541f9f0116daffd1d9c4c5578
Weixuan Zhang <wz315@cam.ac.uk>
2020-01-28 18:34:19 +0000

    Vessel name crawler using selenium

---
+++ datafetcher/vessel.py
@@ -0,0 +1,46 @@
+import json
+import time
+
+from selenium import webdriver
+
+
+def dump_json(data, path):
+    """
+    Save JSON object to file.
+    :param data:
+    :param filename:
+    :return:
+    """
+    with open(path, 'w+') as f:
+        data = json.dump(data, f)
+    return data
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--incognito')
+chrome_options.add_argument('--headless')
+
+driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+
+driver.get('https://www.marinetraffic.com/en/data/?asset_type=vessels&columns=shipname')
+
+time.sleep(2)
+
+driver.find_elements_by_css_selector("button.qc-cmp-button")[1].click()
+
+data = {'names': []}
+
+while True:
+    elements = driver.find_elements_by_css_selector('a.ag-cell-content-link')
+    names = [i.text.lower() for i in elements if i != '']
+
+    print(names)
+
+    data['names'] += names
+
+    try:
+        driver.find_elements_by_css_selector("button[title='Next page']")[0].click()
+    except:
+        break
+
+dump_json(data, './cache/vessel_data.json')
